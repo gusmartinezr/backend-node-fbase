4dc10fc8dfabe3694d56572cfbdb9614eebe022c
Gustavo Martinez Rollano <54968322+gusmartinezr@users.noreply.github.com>
2025-11-05 16:11:12 -0400

    refactor: deploy 2

--- dist/presentation/mappers/user.presenter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toUserResponseDTO = void 0;
-const toUserResponseDTO = (u) => ({
-    id: u.id,
-    email: u.email,
-    createdAt: u.createdAt
-});
-exports.toUserResponseDTO = toUserResponseDTO;
--- dist/presentation/middlewares/error-handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = errorHandler;
-function errorHandler(err, _req, res, _next) {
-    const status = err?.statusCode ?? 500;
-    const code = err?.code ?? "INTERNAL_ERROR";
-    const msg = err?.message ?? "Unexpected error";
-    res.status(status).json({ error: code, message: msg });
-}
